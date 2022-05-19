ae1d94ebe9f418acde0e6b9385f342bc71debc57
adsadilson <adilson.curso@yahoo.com.br>
2022-05-18 22:37:49 -0300

    Criação do serializador phone address

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,15 +1,25 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
-  belongs_to :kind
+  
+  belongs_to :kind do
+    link(:related) {kind_url(object.kind.id)}
+  end
   has_many :phones
   has_one :address
 
   def attributes(*args)
-  h = super(*args)
-  #PT-BR --> h [:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
-  h [:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
-  h
-end
+      h = super(*args)
+      #PT-BR --> h [:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+      h [:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+      h
+  end
+
+  meta do 
+    {author: "Adilson Paraguai"}
+  end
+
+  link(:self) {contact_url(object.id)}
+  link(:kind) {kind_url(object.kind.id)}
 
 
 end
