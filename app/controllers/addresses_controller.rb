73e17a1bc0e3cef0ef3eb11b1aeaec341003c525
adsadilson <adilson.curso@yahoo.com.br>
2022-05-19 23:32:42 -0300

    Configurando as routes para o metodo show

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,15 @@
+class AddressesController < ApplicationController
+    before_action :set_address, only: %i[ show update destroy ]
+
+    # GET /address/1
+    def show
+        render json: @address
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_address
+       @address  = Contact.find(params[:contact_id]).address
+     end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  
   resources :kinds
-  resources :contacts
+
+  resources :contacts do
+    resource :kind, only: [:show]
+    resource :phones, only: [:show]
+    resource :address, only: [:show]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
