8eea378489a70fd96bb0435ff7fc93a96cf66241
adsadilson <adilson.curso@yahoo.com.br>
2022-05-22 22:11:09 -0300

    CRUD AMS com has_one,  has_many (Address, Phones)

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,15 +1,48 @@
 class PhonesController < ApplicationController
-    before_action :set_phones, only: %i[ show update destroy ]
+    before_action :set_phones, only: %i[ show create update destroy ]
 
-    # GET /phones/1
+    # DELETE /contacto/1/phones
+    def destroy
+        phone = Phone.find(phone_params[:id])
+        phone.destroy
+    end
+
+    # POST /contacto/1/phone
+    def create
+        @contact.phones << Phone.new(phone_params)
+
+        if @contact.save
+            render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+        end
+    end
+
+     # PATCH /contacto/1/phone
+     def update
+        phone = Phone.find(phone_params[:id])
+        if phone.update(phone_params)
+            render json: @contact.phones
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+        end
+    end
+    
+    
+    # GET /contacto/1/phones
     def show
-        render json: @phones
+        render json: @contact.phones
     end
 
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_phones
-       @phones  = Contact.find(params[:contact_id]).phones
+       @contact  = Contact.find(params[:contact_id])
      end
+
+    def phone_params
+     ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+    end
+
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   resources :kinds
 
   resources :contacts do
+   
     resource :kind, only: [:show]
+
     resource :phones, only: [:show]
-    resource :address, only: [:show]
+    resource :phone, only: [:update, :create, :destroy]
+
+    resource :address, only: [:show, :update, :create, :destroy]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
